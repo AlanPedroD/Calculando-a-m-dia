4a786b0ce5b9ba2b8361ab286d5594291bb84c17
AlanPedroD <85845887+AlanPedroD@users.noreply.github.com>
2021-12-28 16:55:50 -0300

    Adicionar arquivos de projeto.

---
+++ Calculando a média/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+double n1, n2, n3, n4, res;
+
+n1 = 5.4;
+n2 = 7.2;
+n3 = 8.5;
+n4 = 3.5;
+res = (n1 + n2 + n3 + n4) / 4;
+Console.WriteLine("Olá! a sua méedia é "+res);
